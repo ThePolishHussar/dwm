e15d436fd554d0214d868ee340dd35d178d00dc8
Jakub <jakubb.06@protonmail.com>
2022-12-13 23:32:07 -0500

    make windows with class floating float

--- config.def.h
+++ config.def.h
@@ -42,6 +42,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "firefox", NULL,     NULL,           0,    	  0,          0,           1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "floating",NULL,     NULL,           0,         1,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
